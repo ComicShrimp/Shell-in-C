a4f85d0d3a773d187c4bd917562b245662bb128e
Mário Victor Ribeiro Silva <mvribeiros@hotmail.com>
2018-08-06 22:09:43 -0300

    Todos os arquivos comentados. Projeto Finalizado.

--- Code/arvv.c
+++ Code/arvv.c
@@ -79,36 +79,47 @@ void arvv_libera (ArvVar* a) {
 	free(a);
 }
 
+//Retorna o endereço do diretorio pai do no passado como argumento.
 ArvVar* arv_parent(ArvVar* a){
 	return a->parent;
 }
 
+//Retorna a informação guardada no nó.
 char* arv_info(ArvVar* a){
 	return a->info;
 }
 
+//Retorna o endereço o subdiretorio requisitado através do ponteiro de char.
 ArvVar* arv_subdiretorio(ArvVar* a, char* c){
 
 	ArvVar* aux;
 	for(aux = a->prim;aux != NULL;aux = aux->prox){
+		//Verifica se o valor passado é igual ao no avaliado, e se não é um arquivo.
 		if(!strcmp(aux->info, c) && !aux->arquivo){
 			return aux;
 		}
 	}
 
+	//Caso nenhum no corresponda ao desejado retorna nulo.
 	return NULL;
 }
 
+//Imprime a arvore de diretorios e arquivos.
 void arvv_tree(ArvVar* a){
+
 	ArvVar* p;
 
 	int i = 0;
+	//Imprime o - dependendo no nivel do diretorio ou arquivo.
 	for(i = 0;i < a->nivel;i++){
 		printf("-");
 	}
 
+	//Imprime a informação, no caso o nome, do diretoro ou arquivo
 	printf("%s",a->info);
 	printf("\n");
+
+	//Chama o arvv_tree para todas as subarvores do nó.
 	for(p = a->prim; p != NULL; p = p->prox){
 		arvv_tree(p);
 	}
--- Code/arvv.h
+++ Code/arvv.h
@@ -15,10 +15,14 @@ int arvv_pertence (ArvVar* a, char* c);
 
 void arvv_libera (ArvVar* a);
 
+//Retorna o endereço do diretorio pai do no passado como argumento.
 ArvVar* arv_parent(ArvVar* a);
 
+//Retorna a informação guardada no nó.
 char* arv_info(ArvVar* a);
 
+//Retorna o endereço o subdiretorio requisitado através do ponteiro de char.
 ArvVar* arv_subdiretorio(ArvVar* a, char* c);
 
+//Imprime a arvore de diretorios e arquivos.
 void arvv_tree(ArvVar* a);
